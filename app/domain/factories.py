44d61f506b6b0aeea123a470a28fba30eba55e29
Victor Klapholz <victor.klapholz@gmail.com>
2016-08-04 02:23:24 +0300

    small fixes 2

--- app/domain/factories.py
+++ app/domain/factories.py
@@ -3,7 +3,7 @@ import uuid
 from app.domain.ad_transformers import DO_NOTHING_AD_TRANSFORMER_ID, GOOGLE_AD_TRANSFORMER_ID, FACEBOOK_AD_TRANSFORMER_ID
 from app.domain.ad_transformers import DoNothingAdTransformer, GoogleAdTransformer, FacebookAdTransformer
 
-AD_TRANFORMER_BY_TRANSFORMER_ID = {
+AD_TRANSFORMER_BY_TRANSFORMER_ID = {
     DO_NOTHING_AD_TRANSFORMER_ID: DoNothingAdTransformer,
     GOOGLE_AD_TRANSFORMER_ID: GoogleAdTransformer,
     FACEBOOK_AD_TRANSFORMER_ID: FacebookAdTransformer
@@ -11,10 +11,10 @@ AD_TRANFORMER_BY_TRANSFORMER_ID = {
 
 
 def create_ad_transformer(transformer_id):
-    transfomer = AD_TRANFORMER_BY_TRANSFORMER_ID.get(transformer_id)
+    transfomer = AD_TRANSFORMER_BY_TRANSFORMER_ID.get(transformer_id)
 
     if transfomer is None:
-        raise ValueError("factories.create_ad_tranformer failed because transformer_id='{tranformer_id}' is not supported")
+        raise ValueError("factories.create_ad_transformer failed because transformer_id='{tranformer_id}' is not supported")
 
     transformer_instance = transfomer()
     return transformer_instance
